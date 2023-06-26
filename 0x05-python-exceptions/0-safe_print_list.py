d0c7dbfecc8d2c7ff9ce05aa552d3f657bf99025
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-06-26 07:08:18 -0700

    safe_print_list: handle IndexError only

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
         print("\n", end='')
         return (i + 1)
-    except:
+    except IndexError:
         print("\n", end='')
         return (i)
