215eed2100b6f85b1833219eff066ece67a432ef
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-06 07:55:18 -0700

    6-max_integer_test: initial commit

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_integerList(self):
+        """Test a list of integers.
+        """
+        self.assertEqual(max_integer([10, 2, 30, 4]), 30)
+
+    def test_emptyList(self):
+        """Test an empty list.
+        """
+        self.assertIsNone(max_integer([]))
+
+    def test_oneElementList(self):
+        """Test list of one element.
+        """
+        self.assertEqual(max_integer([0]), 0)
+
+    def test_mixedList(self):
+        """Test a list with mixed types elements.
+        """
+        self.assertRaises(TypeError, max_integer, ['a', 2])
+
+    def test_stringList(self):
+        """Test a list of strings.
+        """
+        self.assertEqual(max_integer(['abc', 'c', 'bb']), 'c')
