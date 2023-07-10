7265298b7025de5458dfe72782ebff1ecb8167be
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-10 13:16:19 -0700

    6-BaseGeometry: raise an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains the class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Class to raise an exception.
+    """
+    def area(self):
+        """Raise an exception with a message indicating this function is empty.
+        """
+        raise Exception("area() is not implemented")
