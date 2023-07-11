64eefed403af28eec84fe844e7738a485bef5ad8
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-11 01:12:28 -0700

    11-square: add string format for square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains the subclass Square of the class Rectangle, which is
+    a subclass of the class BaseGeometry.
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Define a square with area and string format.
+    """
+    def __init__(self, size):
+        """initiate a square object.
+        """
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """compute area of the square.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """string format of the square object.
+        """
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
