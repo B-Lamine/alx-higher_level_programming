2d5b8c9b3c0fd07fd61e54e5a85d688ad08a1452
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-12 09:46:39 -0700

    9-student: define class Student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""
+"""This module contains the class: Student.
 """
 
 
 class Student:
-    """
+    """Student class, with to_json method.
     """
     def __init__(self, first_name, last_name, age):
+        """instantiate Student's first_name, last_name, age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
+    def to_json(self):
+        """get dictionary description of Student.
+        """
+        tmp_dict = self.__dict__
+        dict_obj = {}
+        for key in tmp_dict:
+            if type(tmp_dict[key]) in [list, dict, str, int, bool]:
+                dict_obj[key] = tmp_dict[key]
+        return dict_obj
