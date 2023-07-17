73def33e1ee18a050529918815adc6dcbd47a9ca
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-17 16:00:36 -0700

    Square: create square subclass of rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains the class: Square, which is a subclass of: Rectangle,
+    which in turn is a subclass of: Base.
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class.
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Square class constructor.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string formatted representation of the square class.
+        """
+        return "[Square] (" + str(self.id) + ") " + str(self.x) + "/" +\
+            str(self.y) + " - " + str(self.width)
