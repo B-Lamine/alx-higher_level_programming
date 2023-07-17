ec88b3b6111cdccaa84162874a95a59a545015fe
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-17 15:34:10 -0700

    rectangle: add update method to update all attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,6 +27,20 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+    def update(self, *args):
+        """update attributes depending on the number of arguments.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def __str__(self):
         """string format representation of the rectangle object.
         """
