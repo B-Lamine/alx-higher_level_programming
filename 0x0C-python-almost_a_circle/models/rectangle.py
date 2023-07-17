89f94f1adba4e6c8199669abc180c2e3a87e6416
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-17 14:29:23 -0700

    rectangle: raise adeaute error if type/value of args is incorrect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,14 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """instantiation method.
         """
+        for attr, value in locals().items():
+            if (attr in ['width', 'height', 'x', 'y']) and (type(value)
+                                                            != int):
+                raise TypeError("{} must be an integer".format(attr))
+            if (attr in ['width, height']) and (value <= 0):
+                raise ValueError("{} must be > 0".format(attr))
+            if (attr in ['x', 'y']) and (value < 0):
+                raise ValueError("{} must be >= 0".format(attr))
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -29,6 +37,10 @@ class Rectangle(Base):
     def width(self, value):
         """width stter method
         """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -41,6 +53,10 @@ class Rectangle(Base):
     def height(self, value):
         """height setter method
         """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -53,6 +69,10 @@ class Rectangle(Base):
     def x(self, value):
         """x setter method
         """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -65,4 +85,8 @@ class Rectangle(Base):
     def y(self, value):
         """y setter method
         """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
