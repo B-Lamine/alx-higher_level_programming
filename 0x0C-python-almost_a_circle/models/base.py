8770344d34f400a321c17e169c074b9ad386b445
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-18 14:45:21 -0700

    base: add method to convert json string to list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,6 +27,12 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """convert json string to list.
+        """
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """convert list of instances to json dictionary and save to file.
