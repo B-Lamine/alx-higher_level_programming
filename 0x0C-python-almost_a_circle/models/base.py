9021bb807c42d284abb90e95165bdb56c126a64f
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-18 11:49:34 -0700

    base: add class method to save json list of objects to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,3 +26,16 @@ class Base:
         if (list_dictionaries is None) or (len(list_dictionaries) == 0):
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """convert list of instances to json dictionary and save to file.
+        """
+        if list_objs is None:
+            json_dict = "[]"
+        else:
+            list_objs_dict = [obj.to_dictionary() for obj in list_objs]
+            json_dict = cls.to_json_string(list_objs_dict)
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(json_dict)
