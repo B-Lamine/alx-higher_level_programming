b941d936c552fba2c9e0088464cf8f2fc7e40302
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-07-18 09:46:08 -0700

    square: add update method, to update square attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,6 +28,27 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """update attributes depending on the number of arguments.
+        """
+        if len(args) == 0:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.size = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+
     def __str__(self):
         """string formatted representation of the square class.
         """
