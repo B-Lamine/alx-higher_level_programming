5787032ebd4d25763b2441924c588e851e5aca32
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-08-15 17:19:45 -0700

    9-full_creation: correct values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- create second_table and insert multiples rows
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES(1, "John", 10), (2, "Alex", 13),
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10), (2, "Alex", 3),
        (3, "Bob", 14), (4, "Geogre", 8);
