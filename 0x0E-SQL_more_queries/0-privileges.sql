137aee79aee45aca33581625b43d8ddcdd70aa17
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-08-16 13:39:18 -0700

    flush privileges before listing them

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- script to display priveleges of users
+-- script to display privileges of users
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
