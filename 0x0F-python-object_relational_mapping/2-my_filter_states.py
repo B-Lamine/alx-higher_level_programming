5f4417af4cf07b8ffde4a7e523134462f103374b
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-09-17 14:10:40 -0700

    2-my_filter_states: order multiple results

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,9 +13,11 @@ def db_management(args):
     db = mysql.connect(host="localhost", port=3306, user=args[1],
                        passwd=args[2], db=args[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name='{}'".format(args[4])
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(args[4])
     cur.execute(query)
-    print(cur.fetchone())
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
 
 if __name__ == '__main__':
