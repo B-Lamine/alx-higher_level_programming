cf31651c714b77838c3480cc2a01544ed39b8d82
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-09-17 14:13:13 -0700

    2-filter_states: conform to pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ def db_management(args):
     db = mysql.connect(host="localhost", port=3306, user=args[1],
                        passwd=args[2], db=args[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(args[4])
-    cur.execute(query)
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
+                args[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
