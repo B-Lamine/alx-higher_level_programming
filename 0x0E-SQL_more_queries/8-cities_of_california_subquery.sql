e9c16ad9ed808c525aa2891b566faee93c7137f6
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-09-17 05:53:48 -0700

    8-cities_of_california_subquery: list only id and name fields

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- list all cities of California that can be found in the given database
-SELECT * FROM cities WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY id ASC;
