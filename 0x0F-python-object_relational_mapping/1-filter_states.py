7cfb46976a3fb91f642dda3b9d124d9d27ef971f
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-09-17 13:32:13 -0700

    1-filter_states: select specific state names

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script to query database.
+"""
+
+
+import MySQLdb as mysql
+import sys
+
+
+def db_management(args):
+    """list state names starting with the character 'N'
+    """
+    db = mysql.connect(host="localhost", port=3306, user=args[1],
+                       passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY name DESC")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1].startswith('N'):
+            print(row)
+
+
+if __name__ == '__main__':
+    db_management(sys.argv)
