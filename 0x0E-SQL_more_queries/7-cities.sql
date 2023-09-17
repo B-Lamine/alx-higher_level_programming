993490aeada884470ba801229973b70894e4c8c3
Lamine Bellilet <medlamine.bellilet@outlook.com>
2023-09-17 05:30:42 -0700

    7-cities: create new table in new database and link to toher table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,8 @@
 -- create new table with non-null unique, and auto-generated id column
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT
-        PRIMARY KEY, FOREIGN KEY(state_id) REFEERENCES states(id), name VARCHAR(256));
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY (state_id) REFERENCES states (id));
