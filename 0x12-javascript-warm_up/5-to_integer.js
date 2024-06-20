1a3d1d16dbcf1ca375843f6d221315d173649e09
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-20 16:03:33 -0700

    5-to_integer: initial commit

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (argv[2] && Number(argv[2])) {
+  console.log('My number: ' + parseInt(argv[2]));
+} else {
+  console.log('Not a number');
+}
