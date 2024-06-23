420f22ebe03d43a88d260445f7172b3ec81d20aa
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-23 13:29:37 -0700

    5-filter_cities: create file.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This program runs a query on a database.
+"""
+
+
+import MySQLdb as mysql
+import sys
+
+
+def db_management(args):
+    """Select records from two tables.
+    """
+    db = mysql.connect(host="localhost", port=3306, user=args[1],
+                       passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    db_management(sys.argv)
