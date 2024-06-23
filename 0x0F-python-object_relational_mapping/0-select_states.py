dbe1ac8bf0d5f5aa29fef4bf75672926d21cffeb
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-23 11:12:31 -0700

    0-select_states.py: create file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This program run a SELECT query on a table in the database.
+"""
+
+import MySQLdb as mysql
+import sys
+
+def db_management(args):
+    """Display the table states from given database.
+    """
+    db = mysql.connect(host="localhost", port=3306, user=args[1], passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+if __name__ == "__main__":
+    db_management(sys.argv)
