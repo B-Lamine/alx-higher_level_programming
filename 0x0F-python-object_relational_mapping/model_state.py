ff5457284da88c36e7a41cd7e41be0ba3178c4e3
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-24 07:18:36 -0700

    model_state.py: connect to database only if main

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,13 +11,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
 
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'
-                       .format("root", "root", "3306", "hbtn_0e_6_usa"),
-                       pool_pre_ping=True)
 Base = declarative_base()
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
 
 
 class State(Base):
@@ -31,3 +25,12 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'
+                           .format("root", "root", "3306", "hbtn_0e_6_usa"),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
