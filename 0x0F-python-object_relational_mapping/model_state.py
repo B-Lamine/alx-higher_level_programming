815870beb86a3287d07b1e09835756d0f13ff403
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-24 06:38:20 -0700

    model_state.py: create file.

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This program defines the class State, which is mapped to the table
+   of the same name via SQLAlchemy ORM.
+"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'
+                       .format("root", "root", "3306", "hbtn_0e_6_usa"),
+                       pool_pre_ping=True)
+Base = declarative_base()
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class State(Base):
+    """Maps to the States table of the MySQL database.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
