5467d878718b4e1ba9e3a8afe9e7f8c7e6780636
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-25 12:46:17 -0700

    2-my_filter_states.py: make sql comparison case-sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ def db_management(args):
     db = mysql.connect(host="localhost", port=3306, user=args[1],
                        passwd=args[2], db=args[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM  states WHERE name='{}' ORDER BY\
+    cur.execute("SELECT * FROM  states WHERE BINARY name='{}' ORDER BY\
                 id ASC".format(args[4]))
     rows = cur.fetchall()
     for row in rows:
