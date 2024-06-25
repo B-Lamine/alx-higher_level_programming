19e2d4303cabd4a657f075eea20538dbd769f9f7
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-25 03:49:09 -0700

    7-model_state_fetch_all.py: create file

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script to query all state names in the State table of the given database.
+"""
+
+
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for row in session.query(State).order_by(asc(State.id)):
+        print(str(row.id) + ': ' + row.name)
