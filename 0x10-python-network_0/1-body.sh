ae6a04068633cb1f8484c7ca6e7990141ef3a926
B.Lamine <medlamine.bellilet@outlook.com>
2024-07-05 12:21:26 -0700

    1-body.sh: display response body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # display body of a successful http request
-status_code=$(curl -so /dev/null -w "%{http_code}" "$1")
-if [ "$status_code" -eq 200 ]
-then
-	curl -s "$1"
-fi
+curl -s "$1"
